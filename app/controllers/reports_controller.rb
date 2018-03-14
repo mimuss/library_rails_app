c2ef27cdadebb84ea818fd9f200fcc7ee92db2e0
Nikita <ha4er122@icloud.com>
2018-03-14 15:34:56 +0300

    Reports were changed a little bit Wrote a new request to db which joins library, grants, subscribers and books  tables

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,32 @@
 class ReportsController < ApplicationController
 
+	@@sorted_valeus = {
+		"created_at_desc" => "created_at DESC",
+		"created_at" => "created_at",
+		"name" => "name"
+	}
+
+
 	def index
-		if (!params.has_key?(:date))
+
+		if (!params.has_key?(:date) or !params.has_key?(:sort))
 			render 'index'
 			return
 		end
+
+		@sort_on = params[:sort][:sort_on]
 		@start_on = params[:date][:start_on]
 		@end_on = params[:date][:end_on]
+
+
 		if check_report_book_params
-			@books = Book.joins("INNER JOIN grants ON grants.book_id = books.id").where('grants.date' => Date.new(2001,1,1)..Date.today)
-			puts @books
+
+			@books = Grant.joins(:subscriber, book: :library).where('grants.date' => Date.new(2000,1,1)..Date.today).select('books.*, 
+				grants.date as grants_date, subscribers.name as subcribers_name, 
+				subscribers.surname as subscribers_surname, books.library_id as books_library_id')
+			if params.has_key?(:sort) and @@sorted_valeus.has_key?(params[:sort][:sort_on])
+				@books = @books.order(@@sorted_valeus[sort_params[:sort_on]])
+			end
 		else
 			@error_message = "Неправильно введены даты"
 		end
@@ -18,6 +35,10 @@ class ReportsController < ApplicationController
 
 
 	private
+
+		def sort_params
+			params.require(:sort).permit(:sort_on) 
+		end
 
 		def report_book_params
 			params.require(:date).permit(:start_on, :end_on)
